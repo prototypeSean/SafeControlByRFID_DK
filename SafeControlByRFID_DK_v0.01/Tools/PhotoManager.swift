6da2f7c3ab63ac0527abf934a59322ddf031bb05
DennisKao <denniskao@DennisKaode-MBP.lan>
2019-09-03 17:36:46 +0800

    資料庫測試ＯＫ 未優化

---
+++ SafeControlByRFID_DK_v0.01/Tools/PhotoManager.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoManager.swift
+//  SafeControlByRFID_DK_v0.01
+//
+//  Created by DennisKao on 2019/9/3.
+//  Copyright © 2019 DennisKao. All rights reserved.
+//
+
+import UIKit
+
+class PhotoManager: NSObject {
+
+}
